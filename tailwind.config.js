35c833d8fda807b5446992fea2f1d1d479b30ab0
Scharfcsh <amanadhikari2003@gmail.com>
2024-12-19 22:33:17 +0530

    module export in tailwind conifg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  mode: "jit",
-  theme: {
-    extend: {
-      colors: {
-        primary: "#00040f",
-        secondary: "#00f6ff",
-        dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
-      },
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-      },
+export const content = ["./index.html", "./src/**/*.{js,jsx}"];
+export const mode = "jit";
+export const theme = {
+  extend: {
+    colors: {
+      primary: "#00040f",
+      secondary: "#00f6ff",
+      dimWhite: "rgba(255, 255, 255, 0.7)",
+      dimBlue: "rgba(9, 151, 124, 0.1)",
     },
-    screens: {
-      xs: "480px",
-      ss: "620px",
-      sm: "768px",
-      md: "1060px",
-      lg: "1200px",
-      xl: "1700px",
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
     },
   },
-  plugins: [],
+  screens: {
+    xs: "480px",
+    ss: "620px",
+    sm: "768px",
+    md: "1060px",
+    lg: "1200px",
+    xl: "1700px",
+  },
 };
+export const plugins = [];
